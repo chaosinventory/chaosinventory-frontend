5f49e4cea063402d0831e04685deb5def0c40c44
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-04-22 16:40:59 +0200

    updated package lock, removed some unnecessary stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { authenticationService } from "./services/authenticationService";
 
 function App() {
   return (
