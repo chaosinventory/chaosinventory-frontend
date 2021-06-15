9ed54869dfc336ac54c460cce85e650ae34cf6a8
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-06-15 21:02:37 +0200

    integrated react-table into tagtable

--- src/components/tag/TagTable.jsx
+++ src/components/tag/TagTable.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Spinner, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
-import { getTags } from "../../services/tagService";
+import { deleteTag, getTags } from "../../services/tagService";
 import { useContext } from "react";
 import DataUpdateContext from "../../context/DataUpdateContext";
+import { DeleteButton, EditButton } from "../button/Button";
+import { useTable } from "react-table";
 
 export default function TagTable() {
   const [error, setError] = useState(null);
@@ -10,6 +12,41 @@ export default function TagTable() {
   const [items, setItems] = useState([]);
 
   const { lastUpdate, setLastUpdate } = useContext(DataUpdateContext);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+        Cell: ({ cell: { value } }) => <>#{value}</>,
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Parent",
+        accessor: "parent",
+        Cell: ({ cell: { value } }) => (
+          <>{value ? `${value.name} (#${value.id})` : "..."}</>
+        ),
+      },
+      {
+        Header: "Options",
+        Cell: ({ cell }) => (
+          <>
+            <DeleteButton
+              onClick={() => {
+                deleteTag(cell.row.values.id);
+                setLastUpdate(Date.now());
+              }}
+            />
+          </>
+        ),
+      },
+    ],
+    []
+  );
 
   useEffect(() => {
     getTags().then(
@@ -34,23 +71,40 @@ export default function TagTable() {
   } else if (!isLoaded) {
     return <Spinner />;
   } else {
-    return (
-      <Table variant="simple" size="sm">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Parent</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {items.map((item) => (
-            <Tr key={item.id}>
-              <Td>{item.name}</Td>
-              {item.parent ? <Td>{item.parent.name}</Td> : <Td>...</Td>}
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    );
+    return <TagTableContainer data={items} columns={columns} />;
   }
+}
+
+function TagTableContainer({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  return (
+    <Table variant="simple" size="sm" {...getTableProps()}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th {...column.getHeaderProps()}>{column.render("Header")}</Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>;
+              })}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
 }
